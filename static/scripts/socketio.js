e1127bb67047b8913134ff9723004df069d3a43d
unknown <themonstermora@gmail.com>
2020-12-30 13:11:05 -0500

    Still trying to make it work

--- static/scripts/socketio.js
+++ static/scripts/socketio.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-	var socket = io.connect('https://'+document.domain+':'+location.port);
+	var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 	
 	let room = "Lounge"
     joinRoom("Lounge");
